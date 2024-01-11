2a2e2c9bbf2049669cee5293a4ec0782c585668b
RetchBlade <118535671+RetchBlade@users.noreply.github.com>
2024-01-11 13:45:51 +0100

    Praktikum 4

--- OOP_PraktikumBeispiel_3/src/ownUtil/Observable.java
+++
@@ -1,7 +0,0 @@
-package ownUtil;
-
-public interface Observable {
-	public void addObserver(Observer obs);
-	public void removeObserfer(Observer obs);
-	public void notifyObservers();
-}
---
+++ Praktikum2023_24/OOP_Praktikum_4/test/business/BuergeramtTest.java
@@ -0,0 +1,35 @@
+package business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BuergeramtTest {
+	private Buergeramt bgm;
+	@BeforeEach
+	void setUp() throws Exception {
+		String[] arr = new String[2];
+        arr[0] = "Hauptwohnsitz";
+        arr[1] = "Nebenwohnsitz";
+        this.bgm = new Buergeramt("QuerenBurg", 9.0f, 17.0f, "QuerenburgerHöhe256", arr);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		 this.bgm = null;
+	}
+
+	@Test
+	void test() {
+		 assertTrue(() -> this.bgm.getName().equals("QuerenBurg")); 
+	}
+	
+	@Test
+	void test2() {
+        Throwable exc= assertThrows(IllegalArgumentException.class,() -> {
+        	new Buergeramt("QuerenBurg",9.0f,17.0f,"QuerenburgerHöhe256", null);});
+        assertEquals("Dienstleistungen duerfen nicht null sein.", exc.getMessage());
+    }
+}
