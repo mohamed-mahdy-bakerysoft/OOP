56a05b6a1d41931588f7bb61a483a4b3ab819b2e
RetchBlade <118535671+RetchBlade@users.noreply.github.com>
2024-01-18 13:26:24 +0100

    Praktikum 5 2023

---
+++ Praktikum2023_24/OOP_Praktikum_5/src/gui/guiStaedtischeEinrichtungen/StaedtischeEinrichtungenControl.java
@@ -0,0 +1,26 @@
+package gui.guiStaedtischeEinrichtungen;
+
+import business.buergeramt.BuergeraemterModel;
+import business.sporthallen.SporthallenModel;
+import javafx.stage.Stage;
+import ownUtil.Observer;
+
+public class StaedtischeEinrichtungenControl implements Observer {
+	private BuergeraemterModel buergeraemterModel;
+	private StaedtischeEinrichtungenView staedtischeEinrichtungenView;
+	private SporthallenModel sporthallenModel;
+	public StaedtischeEinrichtungenControl(Stage primaryStage){
+		// TODO Auto-generated method stub
+		// Singleton
+		this.buergeraemterModel = BuergeraemterModel.getInstance();
+		this.sporthallenModel = SporthallenModel.getInstance();
+		this.staedtischeEinrichtungenView = new StaedtischeEinrichtungenView(this, primaryStage, this.buergeraemterModel, this.sporthallenModel);
+		this.buergeraemterModel.addObserver(this);
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		this.staedtischeEinrichtungenView.zeigeBuergeraemterAn();
+	}
+}
